dbcbfc7b2ac0660dce042e65138775647efc1113
lfuaz <lfuaz.dev@gmail.com>
2024-08-15 02:17:03 +0200

    add image for 429 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,34 @@
-import { useEffect, useState,useRef } from 'react'
-import './App.scss'
-import { pokemonNames } from './pokemonList.js'
-import Confetti from 'react-confetti'
+import { useEffect, useState } from 'react';
+import './App.scss';
+import { pokemonNames } from './pokemonList.js';
+import Confetti from 'react-confetti';
+import notFoundPhoto from "./assets/429.webp" 
 
-const randomId = () => Math.floor(Math.random() * 151) + 1
+const randomId = () => Math.floor(Math.random() * 151) + 1;
 
-const capitalize = (str = "") => {
+const capitalize = (str = '') => {
   return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
+};
 
 export const ShowSolution = ({ correctAnswer }) => {
-
   const c = capitalize(correctAnswer);
-  // State to manage the visibility of the solution
   const [showSolution, setShowSolution] = useState(false);
 
-  // Function to toggle the solution visibility
   const toggleSolution = () => {
     setShowSolution(!showSolution);
   };
 
   return (
     <div>
-      {/* Button to show/hide the solution */}
       <button onClick={toggleSolution}>
-        {showSolution ? "Cacher la Solution" : "Afficher la solution"}
+        {showSolution ? 'Cacher la Solution' : 'Afficher la solution'}
       </button>
-      {/* Conditionally render the correct answer */}
       {showSolution && <p>La bonne réponse est : {c}</p>}
     </div>
   );
 };
 
 function App() {
-
-
   const drawPokeball = (ctx) => {
     const paths = [
       { path: "M297.6,380.9c-40.4,0-74.1-28.6-82.1-66.6H81.1c9.5,110.5,102.2,197.2,215.1,197.2s205.7-86.7,215.1-197.2H379.7C371.7,352.4,338,380.9,297.6,380.9z", color: "#FFFFFF" },
@@ -55,172 +48,185 @@ function App() {
     });
     ctx.restore();
   };
-  
-  const [pokemonId, setPokemonId] = useState(randomId())
-  const [currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon-species/" + pokemonId)
-
-  const [userInput, setUserInput] = useState("")
-  const [resultMessage, setResultMessage] = useState("")
-  const [isPokemonFound, setIsPokemonFound] = useState(false)
-  const [descriptionIndex, setDescriptionIndex] = useState(0)
-  const [showType, setShowType] = useState(false)
+  const [pokemonId, setPokemonId] = useState(randomId());
+  const [userInput, setUserInput] = useState('');
+  const [resultMessage, setResultMessage] = useState('');
+  const [isPokemonFound, setIsPokemonFound] = useState(false);
+  const [descriptionIndex, setDescriptionIndex] = useState(0);
+  const [showType, setShowType] = useState(false);
   const [type, setType] = useState([]);
   const [pokemon, setPokemon] = useState({});
 
 
-  useEffect(() => {
-    console.log(type);
-    
-  }, [type])
-
-
-  useEffect(() => {
-    setCurrentUrl("https://pokeapi.co/api/v2/pokemon-species/" + pokemonId)
-  }, [pokemonId])
-
   const handlePokemonChange = () => {
-    setPokemonId(randomId())
-    setResultMessage("") // Reset message
-    setUserInput("") // Reset user input
-    setIsPokemonFound(false) // Reset the found state
-    setDescriptionIndex(0) // Reset the description index
-  }
+    setPokemonId(randomId());
+    setResultMessage(''); // Reset message
+    setUserInput(''); // Reset user input
+    setIsPokemonFound(false); // Reset the found state
+    setDescriptionIndex(0); // Reset the description index
+  };
 
   const removeAllUtf8 = (str) => {
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-  }
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
 
-  const getPokemonDetail = async (url) => {
-    const cachedData = sessionStorage.getItem(url)
+  const getPokemonDetail = async (pokemonId) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`;
+    const cachedData = sessionStorage.getItem(url);
     if (cachedData) {
-      return JSON.parse(cachedData)
+      return JSON.parse(cachedData);
     }
-    const response = await fetch(url)
-    const data = await response.json()
+    const response = await fetch(url);
+    const data = await response.json();
 
-    const name = removeAllUtf8(data.names.filter((name) => name.language.name === "fr")[0].name).toLowerCase()
+    const name = removeAllUtf8(
+      data.names.filter((name) => name.language.name === 'fr')[0].name
+    ).toLowerCase();
 
-    const descriptions = data.flavor_text_entries.filter((entry) => {
-      return entry.language.name === "fr" && !entry.flavor_text.toLowerCase().includes(removeAllUtf8(name).toLowerCase())
-    }).map((entry) => {
-      return {
-        flavor_text: capitalize(removeAllUtf8(entry.flavor_text).toLowerCase())
-      }
-    })
+    const descriptions = data.flavor_text_entries
+      .filter((entry) => {
+        return (
+          entry.language.name === 'fr' &&
+          !entry.flavor_text.toLowerCase().includes(removeAllUtf8(name).toLowerCase())
+        );
+      })
+      .map((entry) => {
+        return {
+          flavor_text: capitalize(removeAllUtf8(entry.flavor_text).toLowerCase()),
+        };
+      });
 
-    const uniqueDescriptions = [...new Set(descriptions.map(item => item.flavor_text))]
+    const uniqueDescriptions = [...new Set(descriptions.map((item) => item.flavor_text))];
 
     const result = {
       name: name,
-      descriptions: uniqueDescriptions
-    }
+      descriptions: uniqueDescriptions,
+    };
 
-    sessionStorage.setItem(url, JSON.stringify(result))
-    return result
-  }
+    sessionStorage.setItem(url, JSON.stringify(result));
+    return result;
+  };
 
-  const getTypeOfPokemon = async (url) => {
-    const response = await fetch(url);
+  const getTypeOfPokemon = async (pokemonId) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     const data = await response.json();
     const type = data.types.map((type) => type.type.name);
     setType(type);
   };
 
-
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getPokemonDetail(currentUrl);
-      await getTypeOfPokemon("https://pokeapi.co/api/v2/pokemon/" + pokemonId);
+      const data = await getPokemonDetail(pokemonId);
+      await getTypeOfPokemon(pokemonId);
 
       setPokemon({
         descriptions: data.descriptions,
         name: data.name,
-        photo: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`
+        photo: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`,
       });
     };
     fetchData();
-  }, [currentUrl, pokemonId]); // Add pokemonId to the dependency array
+  }, [pokemonId]); // Now only dependent on pokemonId
 
   const handleInputChange = (event) => {
-    setUserInput(event.target.value)
-  }
+    setUserInput(event.target.value);
+  };
 
   const handleSubmit = () => {
     if (userInput.toLowerCase() === pokemon.name) {
-      setResultMessage("Bravo ! Vous avez trouvé le Pokémon !")
-      setIsPokemonFound(true)
+      setResultMessage('Bravo ! Vous avez trouvé le Pokémon !');
+      setIsPokemonFound(true);
     } else {
-      setResultMessage("Incorrect ! Essayez encore ou demandez une autre description.")
+      setResultMessage('Incorrect ! Essayez encore ou demandez une autre description.');
     }
-  }
+  };
 
   const handleNextDescription = () => {
     if (descriptionIndex < pokemon.descriptions.length - 1) {
-      setDescriptionIndex(descriptionIndex + 1)
+      setDescriptionIndex(descriptionIndex + 1);
     }
-  }
+  };
 
   return pokemon.descriptions ? (
     <div className="App">
       <h1>Devinez le Pokémon !</h1>
       {isPokemonFound && (
-        <img src={pokemon.photo} alt={pokemon.name} />
-      )}
-        <p className="description">{pokemon.descriptions[descriptionIndex]}</p>
-     <div className='indice-wrapper'>
-        <button className='w-50' onClick={handleNextDescription} disabled={descriptionIndex >= pokemon.descriptions.length - 1}>
+  <img 
+    src={pokemon.photo} 
+    alt={pokemon.name} 
+    onError={(e) => {
+      if (!e.target.dataset.fallback) {
+        e.target.src = notFoundPhoto;
+        e.target.dataset.fallback = "true";
+      }
+    }} 
+  />
+)}
+      <p className="description">{pokemon.descriptions[descriptionIndex]}</p>
+      <div className="indice-wrapper">
+        <button
+          className="w-50"
+          onClick={handleNextDescription}
+          disabled={descriptionIndex >= pokemon.descriptions.length - 1}
+        >
           Une autre descriptions
-        </button> 
-        <button className={showType ? "bi-color w-50" : "w-50"} style={{
-          "--type_1": `var(--${type[0]})`,
-          "--type_2": `var(--${type[1] || type[0]})`,
-        }} onClick={() => setShowType(!showType)}>
-          <p className='type'>{!showType ? "Type ? " : type.map(
-            (type, index) => (
-              <span key={index} className="type">{type}</span>
-            )
-          )}</p>
-        </button> 
+        </button>
+        <button
+          className={showType ? 'bi-color w-50' : 'w-50'}
+          style={{
+            '--type_1': `var(--${type[0]})`,
+            '--type_2': `var(--${type[1] || type[0]})`,
+          }}
+          onClick={() => setShowType(!showType)}
+        >
+          <p className="type">
+            {!showType
+              ? 'Type ? '
+              : type.map((type, index) => (
+                  <span key={index} className="type">
+                    {type}
+                  </span>
+                ))}
+          </p>
+        </button>
       </div>
       <br />
       <div className="answer-container">
-        
-      <input
-        type="text"
-        value={userInput}
-        onChange={handleInputChange}
-        placeholder="Pokémon"
-        list="pokemon-list"
-      />
-      <datalist id="pokemon-list">
-        {pokemonNames.map((name, index) => (
-          <option key={index} value={name} />
-        ))}
-      </datalist>
-      <button onClick={handleSubmit}>Valider</button>
+        <input
+          type="text"
+          value={userInput}
+          onChange={handleInputChange}
+          placeholder="Pokémon"
+          list="pokemon-list"
+        />
+        <datalist id="pokemon-list">
+          {pokemonNames.map((name, index) => (
+            <option key={index} value={name} />
+          ))}
+        </datalist>
+        <button onClick={handleSubmit}>Valider</button>
       </div>
       <p>{resultMessage}</p>
-      {resultMessage === "Bravo ! Vous avez trouvé le Pokémon !" ? (
+      {resultMessage === 'Bravo ! Vous avez trouvé le Pokémon !' ? (
         <>
-       <Confetti
-          width={innerWidth}
-          height={innerHeight}
-          numberOfPieces={80}
-          friction={1}
-          initialVelocityY={0}
-          initialVelocityX={0}
-          tweenDuration={10000}
-          drawShape={ctx => drawPokeball(ctx)}
-        />
-        <button onClick={handlePokemonChange}>Passer au suivant</button>
+          <Confetti
+            width={innerWidth}
+            height={innerHeight}
+            numberOfPieces={80}
+            friction={1}
+            initialVelocityY={0}
+            initialVelocityX={0}
+            tweenDuration={10000}
+            drawShape={(ctx) => drawPokeball(ctx)}
+          />
+          <button onClick={handlePokemonChange}>Passer au suivant</button>
         </>
       ) : (
         <button onClick={handlePokemonChange}>Passer à un autre Pokémon</button>
       )}
-            <ShowSolution correctAnswer={pokemon.name} />
-
+      <ShowSolution correctAnswer={pokemon.name} />
     </div>
-  ) : null
+  ) : null;
 }
 
-export default App
+export default App;
