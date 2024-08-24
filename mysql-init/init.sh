e4eb8ff9b14cd0007fff1661aa100c4250f68908
Florian Urena <lfuaz.dev@gmail.com>
2024-08-25 01:49:08 +0200

    fix

--- mysql-init/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace placeholders with environment variable values
-envsubst < /docker-entrypoint-initdb.d/init.sql.template > /docker-entrypoint-initdb.d/init.sql
-
-# Execute the SQL script
-exec "$@"
